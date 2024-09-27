72d728b7db9c879428ed4f93de9b3bd597f098d3
rahulpatelsprp <rahulpatelsprp@gmail.com>
2024-09-27 14:06:26 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,8 +3,25 @@ from itertools import pairwise
 
 class Solution(object):
     def romanToInt(self, s):
-        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        return sum((-1 if d[a] < d[b] else 1) * d[a] for a, b in pairwise(s)) + d[s[-1]]
+         m = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        ans = 0
+        
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+        
+        return ans
 
 
 
